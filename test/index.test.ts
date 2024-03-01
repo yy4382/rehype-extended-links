20de5d4097cb17f4fb37a77a749e64fa90953400
Chris Yang <yy4382@outlook.com>
2024-03-01 22:25:38 +0800

    test: more tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -3,9 +3,13 @@ import type { Options } from "../lib/index.ts";
 import { assert } from "chai";
 import { rehype } from "rehype";
 
-describe("Examples", () => {});
+describe("Package", () => {
+  it('should expose the public api', () => {
+    assert.isFunction(rehypeExtendedLinks);
+  });
+});
 
-describe("Extended links Tests", () => {
+describe("Link type check", () => {
   // input: <a href="http://example.com">?</a>
   // expected: <a href="http://example.com" rel="nofollow">?</a>
   it("should not change a relative link", async () => {
@@ -15,6 +19,25 @@ describe("Extended links Tests", () => {
     assert.equal(result, output);
   });
 
+
+  it("should not change a fragment link", async () =>{
+    const input = '<a href="#example">fragment</a>'
+    const expected = '<a href="#example">fragment</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
+  it("should not change a search link", async () =>{
+    const input = `<a href="?search">search</a>`
+    const expected = '<a href="?search">search</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
+  it("should not change a mailto link", async () =>{
+    const input = '<a href="mailto:a@b.com">mailto</a>'
+    const expected = '<a href="mailto:a@b.com">mailto</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
   // input: <a href="//example.com">?</a>
   // expected: <a href="//example.com" rel="nofollow">?</a>
   it("should change a protocol-relative link", async () => {
@@ -23,7 +46,26 @@ describe("Extended links Tests", () => {
     const output = `<a href="//example.com" rel="nofollow">?</a>`;
     assert.equal(result, output);
   });
-
+  it("should change a http link", async () =>{
+    const input = '<a href="http://example.com">http</a>'
+    const expected = '<a href="http://example.com" rel="nofollow">http</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
+  it("should change a https link", async () =>{
+    const input = '<a href="https://example.com">https</a>'
+    const expected =  '<a href="https://example.com" rel="nofollow">https</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
+  it("should not change a www link", async () =>{
+    const input = '<a href="www.example.com">www</a>'
+    const expected =  '<a href="www.example.com">www</a>'
+    const result = await process(input, {});
+    assert.equal(result, expected);
+  })
+})
+describe("Options check", () => {
   it("should wrap in span because content exist", async () => {
     const input = `<a href="//example.com">?</a>`;
     const options: Options = {
@@ -86,16 +128,28 @@ describe("Dynamically generated tests", () => {
       };
     },
   };
-  it("should add preContent", async () => {
+  it("should add preContent to a link that matches the test function", async () => {
     const input = `<a href="https://github.com/abc">?</a>`;
     const result = await process(input, dynPreContentOptions);
     const output = `<span class="wrapped-link-container"><span><svg><use href="#mdi--github"></use></svg></span><a href="https://github.com/abc" class="wrapped-link" rel="nofollow">?</a></span>`;
     assert.equal(result, output);
   });
-  it("should not add preContent", async () => {
+  it("should not add preContent to a link that matches the test function", async () => {
     const input = `<a href="https://example.com">?</a>`;
     const result = await process(input, dynPreContentOptions);
     const output = `<a href="https://example.com" rel="nofollow">?</a>`;
+    assert.equal(result, output);
+  });
+
+  it('should add rel to a link that matches the test function', async () => {
+    const input = '<a href="http://example.com">http</a>';
+    const options: Options = {
+      test: (node) => {
+        return node.properties.href === "http://example.com";
+      },
+    };
+    const output = '<a href="http://example.com" rel="nofollow">http</a>';
+    const result = await process(input, options);
     assert.equal(result, output);
   });
 
